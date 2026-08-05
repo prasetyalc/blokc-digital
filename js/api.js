00a98a0bc2bbd8ca6aa086a2a3cc3b82eec2f431
Danang Adhi <prasetyaadhi27@gmail.com>
2026-08-05 08:28:42 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -6,7 +6,7 @@
 
 // GANTI dengan URL Web App hasil deploy Apps Script kamu, contoh:
 // "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec"
-const API_URL = "https://script.google.com/macros/u/1/s/AKfycbzYjJCrZ9zX36IPIZRKPUda8iwDgyiea3anAIGUrHhqsjmXHcWbXp_4C33mhC_8atyp/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzYjJCrZ9zX36IPIZRKPUda8iwDgyiea3anAIGUrHhqsjmXHcWbXp_4C33mhC_8atyp/exec";
 
 const RTApi = (() => {
 
